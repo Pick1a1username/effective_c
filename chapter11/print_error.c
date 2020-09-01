11068b0bf0e42d87c93daa893ff1877a3779d545
Pick1a1username <xscreensaver@outlook.com>
2020-09-01 12:50:36 +0900

    20200901_0

---
+++ chapter11/print_error.c
@@ -0,0 +1,27 @@
+// Listing 11-6
+
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <malloc.h>
+
+errno print_error(errno errnum) {
+  rsize_t size = strerrorlen_s(errnum);
+  char* msg = malloc(size);
+  if ((msg != NULL) && (strerror_s(msg, size, errnum) != 0)) {
+    fputs(msg, stderr);
+    return 0;
+  }
+  else {
+    fputs("unknown error", stderr);
+    return ENOMEM;
+  }
+}
+
+int main(void) {
+  print_error(ENOMEM);
+  exit(1);
+}
+
